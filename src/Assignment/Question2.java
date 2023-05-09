431a4ad960e5ce69606b45d1e0226bee52d1e14d
RohanPhuyalZ <rohanphuyal2021@gmail.com>
2023-05-09 13:48:04 +0545

    q2

--- src/Assignment/Question2.java
+++ src/Assignment/Question2.java
@@ -2,21 +2,27 @@ package Assignment;
 
 import java.awt.*;
 
-public class Question1 {
-    Question1(){
+public class Question2 {
+    Question2(){
         Frame frame = new Frame();
         frame.setSize(600,600);
         Panel panel = new Panel(){
             public void paint(Graphics g){
                 super.paint(g);
                 g.drawLine(10,20,100,200);
+                g.drawOval(10,220,200,150);
                 g.drawRect(150,20,190,130);
                 g.drawOval(350,20,200,200);
-                g.drawOval(10,220,200,150);
-//                int []x= {240,240,360};
-//                int []y= {240,360,360};
-//                g.drawPolygon(x,y,3);
                 g.drawPolygon(new int[] {240,240,360}, new int[] {240,360,360}, 3);
+
+                g.setColor(Color.RED);
+                g.fillRect(150, 20, 190,130);
+                g.setColor(Color.BLUE);
+                g.fillOval(350,20,200,200);
+                g.setColor(Color.GREEN);
+                g.fillOval(10,220,200,150);
+                g.setColor(Color.CYAN);
+                g.fillPolygon(new int[] {240,240,360}, new int[] {240,360,360}, 3);
             }
         };
         frame.add(panel);
@@ -24,6 +30,6 @@ public class Question1 {
     }
 
     public static void main(String[] args) {
-        new Question1();
+        new Question2();
     }
 }
